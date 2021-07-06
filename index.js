a2d77a742d7019607819cb0b8984eae8fa543df2
marcio <marcioqui3@gmail.com>
2021-07-06 12:03:26 +0100

    updated origns

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const pdfConvertor = require("./routes/pdfconvertor");
 app.use(function (req, res, next) {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://loving-albattani-a66e95.netlify.app/"
+    "https://loving-albattani-a66e95.netlify.app"
   ); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
